3c0b59a1435ea5e988563d91c9249c950640bfcb
Matthias Hempel <7202441+fotopixel@users.noreply.github.com>
2026-02-27 16:20:22 +0100

    init

---
+++ apps/host/dist/app/nx-welcome.d.ts
@@ -0,0 +1,5 @@
+export declare function NxWelcome({ title }: {
+    title: string;
+}): import("@emotion/react/jsx-runtime").JSX.Element;
+export default NxWelcome;
+//# sourceMappingURL=nx-welcome.d.ts.map
